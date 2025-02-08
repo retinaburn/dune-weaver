94b01bd7ca990836a68633c55e47b36d29b9b8ad
Craig Moynes <craig.moynes@gmail.com>
2025-02-08 17:26:10 -0500

    append newline to message before sending to make count more accurate

--- app.py
+++ app.py
@@ -87,7 +87,8 @@ DELIVERY_METHOD = "mqtt" # or "serial"
 
 def publish(message, ser=None):
     if DELIVERY_METHOD == "mqtt":
-        mqtt.publish(TOPIC, message+"\n", qos=2)
+        message += "\n"
+        mqtt.publish(TOPIC, message, qos=2)
         print(f"Sent: {len(message)} bytes")
     else:
         with serial_lock:
