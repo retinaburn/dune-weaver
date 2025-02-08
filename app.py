1ee9013142081bfbde226f9d2d93bbe02a90c288
Craig Moynes <craig.moynes@gmail.com>
2025-02-08 10:28:19 -0500

    loop forever

--- app.py
+++ app.py
@@ -77,8 +77,9 @@ mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
 print("Connecting to MQTT broker...")
-mqttc.connect("192.168.1.224", 1883)
+mqttc.connect("192.168.1.224")
 print("Connected to MQTT broker")
+mqttc.loop_forever()
 
 
 def get_ino_firmware_details(ino_file_path):
