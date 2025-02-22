eee8de77e0ed3dfeb09eb5e86e2c928a768afd95
Craig Moynes <craig.moynes@gmail.com>
2025-02-21 21:55:24 -0500

    potential new code

--- dune_weaver_flask/modules/core/pattern_manager.py
+++ dune_weaver_flask/modules/core/pattern_manager.py
@@ -143,7 +143,25 @@ def interpolate_path(theta, rho):
     new_x_abs = state.machine_x + x_increment
     new_y_abs = state.machine_y + y_increment
     
-    # dynamic_speed = compute_dynamic_speed(rho, max_speed=state.speed)
+    #
+    # 
+    # x_scaling_factor = 2
+    # y_scaling_factor = 5
+    
+    # delta_theta = theta - state.current_theta
+    # delta_rho = rho - state.current_rho
+    # x_increment = delta_theta * 100 / (2 * pi * x_scaling_factor) # Scale down x from 100mm to 50mm per revolution
+    # y_increment = delta_rho * 100 / y_scaling_factor # Scale down y from 100mm to 20mm from center to perimeter
+    
+    # x_total_steps = state.x_steps_per_mm * (100/x_scaling_factor)
+    # y_total_steps = state.y_steps_per_mm * (100/y_scaling_factor)
+        
+    # x_increment / 50 * (x_total_steps)
+    # offset = x_increment * (x_total_steps * x_scaling_factor / (state.gear_ratio * y_total_steps * y_scaling_factor))
+    # y_increment += offset
+    
+    # new_x_abs = state.machine_x + x_increment
+    # new_y_abs = state.machine_y + y_increment    
     
     serial_manager.send_grbl_coordinates(round(new_x_abs, 3), round(new_y_abs,3), state.speed)
     state.current_theta = theta
